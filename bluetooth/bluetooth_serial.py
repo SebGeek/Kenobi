966c77fdbfaa9a7a558a6beb2622bad92a26ccf2
seb <famille.auray@gmail.com>
2016-05-26 22:34:27 +0200

    Update Bluetooth serial

--- bluetooth/bluetooth_serial.py
+++ bluetooth/bluetooth_serial.py
@@ -1,56 +1,70 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-import bluetooth
+import os
+import time
+import serial
+import subprocess
 
 '''
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo rpi-update
+
 B8:27:EB:83:9E:E9       raspberrypi3
-00:1D:DF:FE:B0:A9       n/a Sculpt Touche Mouse ?
+00:1D:DF:FE:B0:A9       PHILIPS HTL4110B barre de son
 C0:D9:62:04:84:DF       PC-SEB
 98:D6:F7:71:8C:53       Nexus 4
 
-sudo bluetoothctl
+Pour appairer le téléphone
+sudo bluetoothctl -a
 power on
+discoverable yes
+pairable on
+scan on
+(wait to see Nexus 4)
+scan off
 agent on
 default-agent
-scan on
 pair 98:D6:F7:71:8C:53
+trust 98:D6:F7:71:8C:53
+connect 98:D6:F7:71:8C:53 => il est normal que la connection se fasse puis se deconnecte (2 secondes après) car le téléphone ne trouve pas de Serial Protocol SP sur le Raspberry Pi
+exit
 
-I am using bluetooth to connect to serial on python so I simply run sudo rfcomm bind 0 xx:xx:xx:xx:xx:xx via subprocess.call to created a serial port.
-Make sure to run sudo rfcomm release 0 at the end of the script to release the serial port
-sudo rfcomm bind 0 98:D6:F7:71:8C:53
-sudo rfcomm release 0
-
+sudo nano /etc/systemd/system/bluetooth.target.wants/bluetooth.service
+    ExecStart=/usr/lib/bluetooth/bluetoothd --noplugin=sap --compat
+    ExecStartPost=/usr/bin/sdptool add SP
+sudo rfcomm listen hci0&
+=> à ce moment là, on peut se connecter avec le telephone
+sudo minicom rfcomm0
 
 '''
 
+
 if __name__ == '__main__':
 
+    command = "sudo rfcomm listen hci0 >/dev/null &"
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    time.sleep(0.5)
 
-    if len(sys.argv) < 2:
-            print "usage: sdp-browser <addr>"
-            print "   addr can be a bluetooth address, \"localhost\" or \"all\""
-            sys.exit(2)
-
-    target = sys.argv[1]
-    if target == "all" : target = None
-
-    services = bluetooth.find_service(address=target)
-
-    if len(services) > 0:
-            print "found %d services on %s" % (len(services), sys.argv[1])
-            print
+    try:
+        ser = serial.Serial('/dev/rfcomm0', 115200)  # open first serial port
+    except:
+        print "no device connected"
     else:
-            print "no services found"
+        print "device connected"
+        ser.write("hello from Pi\r\n")  # write a string
 
-    for svc in services:
-            print "Service Name: %s" % svc["name"]
-            print "  Host: %s" % svc["host"]
-            print "  Description: %s" % svc["description"]
-            print "  Provided By: %s" % svc["provider"]
-            print "  Protocol: %s" % svc["protocol"]
-            print "  channel/PSM: %s" % svc["port"]
-            print "  svc classes: %s" % svc["service-classes"]
-            print "  profiles: %s" % svc["profiles"]
-            print "  service id: %s" % svc["service-id"]
+        cmd_recv = ""
+        while "END" not in cmd_recv:
+            try:
+                cmd_recv = ser.readline()
+            except:
+                print "error in serial read"
+                break
+            else:
+                print cmd_recv,
+                ser.write("Pi received:" + cmd_recv)  # write a string
+
+        ser.close()  # close port
