c0353b39b0f27f3f6c1ac0a8ee3443b61e13951e
seb <famille.auray@gmail.com>
2016-05-30 20:59:51 +0200

    Update motor

--- motor/motor.py
+++ motor/motor.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import time
-
 from rrb3 import *
 
+'''
+https://github.com/simonmonk/raspirobotboard3
+'''
 
 if __name__ == '__main__':
-    rr = RRB3(9, 6)
-    rr.set_led1(1)
-    time.sleep(2)
-    rr.set_led1(0)
-    time.sleep(2)
-    rr.set_led2(1)
-    time.sleep(2)
-    rr.set_led2(0)
-    time.sleep(2)
-    print rr.sw1_closed()
+    rr = RRB3(7.4, 6) # battery voltage is 7.4V, max voltage for motors is 6.0V
+
+    print rr.get_distance()
+
+    motor_speed = 0.6
+    rr.forward(1, motor_speed)
+    rr.right(0.5, motor_speed)
+    rr.forward(1, motor_speed)
+    rr.left(0.5, motor_speed)
+    rr.forward(2, motor_speed)
+    rr.right(0.5, motor_speed)
+    rr.forward(2, motor_speed)
+    rr.left(0.5, motor_speed)
