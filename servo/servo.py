5f70b9ecbc1d3dd0ea860ab4e8adbf1852e939ad
seb <famille.auray@gmail.com>
2016-06-15 22:51:28 +0200

    Servo pan/tilt

--- servo/servo.py
+++ servo/servo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import os
 import time
 import multiprocessing
@@ -7,16 +8,17 @@ from Queue import Queue, Empty
 
 class ThreadMoveServo(multiprocessing.Process):
     ''' Create a thread '''
-    def __init__(self, com_queue_RX, com_queue_TX, val=8, up=True, speed=0.01):
-        self.update(val, up, speed)
+    def __init__(self, com_queue_RX, com_queue_TX, val=8, up=False, speed=0.01):
         self.com_queue_RX = com_queue_RX
         self.com_queue_TX = com_queue_TX
-    
         self.RqTermination = False
+
+        self.update(val, up, speed)
 
         # servod is compiled from ServoBlaster GITHub
         # https://github.com/richardghirst/PiBits/tree/master/ServoBlaster
-        os.system('sudo ./servo/servod')
+        # servos must be connected on pins 35 (GPIO19) and 36 (GPIO16)
+        os.system('sudo /home/pi/Kenobi/servo/servod --p1pins="35,36" > /dev/null')
         
         super(ThreadMoveServo, self).__init__()
     
@@ -30,19 +32,11 @@ class ThreadMoveServo(multiprocessing.Process):
     
     def run(self):
         while self.RqTermination == False:
-            if self.val == 92:
-                self.up = False
-            elif self.val == 8:
-                self.up = True
-                
+            # 0=vertical   / 73=down / mid=35 / 1=up
+            # 1=horizontal / 3=right / mid=40 / 80=left
             os.system("echo 0=" + str(self.val) + "% > /dev/servoblaster")
-            time.sleep(self.speed)
-            
-            if self.up == True:
-                self.val += 1
-            else:
-                self.val -= 1
-            
+            time.sleep(0.2)
+
             ''' read com_queue_RX '''
             try:
                 com_msg = self.com_queue_RX.get(block=True, timeout=None)
@@ -54,12 +48,38 @@ class ThreadMoveServo(multiprocessing.Process):
                     self.RqTermination = True
                 elif com_msg[0] == "UPDATE":
                     (val, up, speed) = com_msg[1]
-                    self.update(val, up, float(speed))
+                    self.update(val, up, speed)
                 else:
                     print "unknown msg"
 
+def change_angle(pwm, angle):
+    dutycycle = ((angle / 180.0) + 1.0) * 5.0
+    pwm.ChangeDutyCycle(dutycycle)
 
 if __name__ == '__main__':
+    '''
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(19, GPIO.OUT)
+    # PWM on port GPIO_pin at 50 Hertz (20ms)
+    pwm_vertical = GPIO.PWM(19, 50)
+    pwm_horizontal = GPIO.PWM(16, 50)
+    dutycycle = ((90 / 180.0) + 1.0) * 5.0
+    pwm_vertical.start(dutycycle)
+    pwm_horizontal.start(dutycycle)
+
+    while True:
+        angle = raw_input("pwm_vertical: angle ?")
+        change_angle(pwm_vertical, int(angle))
+        time.sleep(1)
+
+    pwm_vertical.stop()
+    pwm_horizontal.stop()
+    GPIO.cleanup()
+    '''
+
     ThreadMoveServo_com_queue_TX = multiprocessing.Queue()
     ThreadMoveServo_com_queue_TX.cancel_join_thread()
     ThreadMoveServo_com_queue_RX = multiprocessing.Queue()
@@ -69,12 +89,12 @@ if __name__ == '__main__':
     ThreadMoveServo.start()
     
     while True:
-        speed = raw_input("speed ? (in sec - 0 to exit)")
+        value = raw_input("val ? (in sec - 0 to exit)")
         
-        if speed == "0":
+        if value == "0":
             print "stop request"
             ThreadMoveServo_com_queue_RX.put(("STOP",))
             ThreadMoveServo.join() # Wait until thread terminates
             break
         
-        ThreadMoveServo_com_queue_RX.put(("UPDATE", (None, None, speed)))
+        ThreadMoveServo_com_queue_RX.put(("UPDATE", (int(value), None, None)))
