9ff9b1cefaed7cc6a18b0d816fb6a53ca31e4114
seb <famille.auray@gmail.com>
2017-05-13 21:53:13 +0200

    up

---
+++ assistant/assistant.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/config-dev-project-and-account
+
+https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/configure-audio
+
+sudo apt-get update
+sudo apt-get install python3-dev python3-venv
+python3 -m venv env
+env/bin/python -m pip install pip setuptools --upgrade
+source env/bin/activate
+
+python -m googlesamples.assistant.auth_helpers --client-secrets /home/pi/client_secret_xxx.googleusercontent.com.json
+
+python -m googlesamples.assistant
+
+https://console.developers.google.com/apis/api/embeddedassistant.googleapis.com/overview?project=api-assistant
+
+cd /home/pi/env/lib/python3.4/site-packages/googlesamples/assistant
+
+
+Start assistant:
+source env/bin/activate
+python -m googlesamples.assistant
+
+'''
--- assistant/led_button.py
+++ assistant/led_button.py
@@ -4,19 +4,33 @@
 import RPi.GPIO as GPIO
 import time
 
+# Button connected between GND and GPIO#18
+# LED connected between GND and GPIO#23 through a 440 ohm resistor
+led_state = False
+
+# Our function on what to do when the button is pressed
+def shutdown(gpio_number):
+    global led_state
+
+    # os.system("sudo shutdown -h now")
+    led_state = not led_state
+    GPIO.output(23, led_state)
+
 if __name__ == '__main__':
     print "led and button"
-
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(23, GPIO.OUT)
 
+    # Add our function to execute when the button pressed event happens
+    GPIO.add_event_detect(18, GPIO.FALLING, callback=shutdown, bouncetime=200)
+
     while True:
         input_state = GPIO.input(18)
         if input_state == True:
-            GPIO.output(23, False)
             print "continue"
         else:
-            GPIO.output(23, True)
+            # BUTTON PRESSED
             print "pass"
         time.sleep(0.5)
