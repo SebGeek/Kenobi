1bb8b0071c8aab61ba738b428b9409f4f29daf6c
seb <famille.auray@gmail.com>
2017-05-16 22:12:20 +0200

    up

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -2,10 +2,51 @@
 # -*- coding: utf-8 -*-
 
 '''
-https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/config-dev-project-and-account
+1. https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/config-dev-project-and-account
 
-https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/configure-audio
+2. https://developers.google.com/assistant/sdk/prototype/getting-started-pi-python/configure-audio
+Verify that recording and playback work:
 
+# Play a test sound (this will be a person speaking). Press Ctrl+C when done.
+# If you don't hear anything when you run this, check your speaker connection.
+speaker-test -t wav
+
+# Record a short audio clip.
+arecord --format=S16_LE --duration=5 --rate=16k --file-type=raw out.raw
+# Check the recording by replaying it.
+aplay --format=S16_LE --rate=16k out.raw
+
+# Adjust the playback and recording volume.
+alsamixer
+If recording and playback are working, then you are done configuring audio. If not (or if you receive an error), continue to the next step below.
+Find your recording and playback devices.
+
+# Note the card number and device number for recording.
+arecord -l
+# Note the card number and device number for playback.
+aplay -l
+Create a new file named .asoundrc in the home directory (/home/pi). Make sure it has the right slave definitions for microphone and speaker:
+
+pcm.!default {
+  type asym
+  capture.pcm "mic"
+  playback.pcm "speaker"
+}
+pcm.mic {
+  type plug
+  slave {
+    pcm "hw:<card number>,<device number>"
+  }
+}
+pcm.speaker {
+  type plug
+  slave {
+    pcm "hw:<card number>,<device number>"
+  }
+}
+
+
+3.
 sudo apt-get update
 sudo apt-get install python3-dev python3-venv
 python3 -m venv env
